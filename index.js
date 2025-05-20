0a90f528e792f290a0491398e828c3af68ee12ad
NaveenRathore5094 <rathorenaveen121@gmail.com>
2025-05-20 12:19:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/telegram", (req, res) => {
+  res.send("naveen_rathore.007");
+});
+app.get("/login", (req, res) => {
+  res.send("<h1>Please Login in telegram</h1>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
